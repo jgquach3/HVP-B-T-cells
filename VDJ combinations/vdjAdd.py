63b9dbe241958da3bc60688db327eda7fa310a14
Jimmy Quach <jquach3@gmail.com>
2017-10-18 10:59:04 -0800

    fix mind/mini

--- VDJ combinations/vdjAdd.py
+++ VDJ combinations/vdjAdd.py
@@ -151,8 +151,8 @@ def readingFrameChecker(seq):
 #specifically used for deletions
 def thresholdSet(l, start, end):
 
-    r = random.randint(start, end)
     for i in range(len(l)):
+        r = random.randint(start, end)
         if l[i] <= -1:
             l[i] = r
             
@@ -162,7 +162,7 @@ def thresholdSet(l, start, end):
 #ie vd insertion = 5 -> v gets 3 nucleotides and d get 2 nucleotides
 #randomly assigns a value to both
 #specifically used for additions
-def insertionDealer(insertion, maxI, minI):
+def insertionDealer(insertion, minI, maxI):
 
     if insertion < 0:
         r = random.randint(minI, maxI)
@@ -234,21 +234,22 @@ def rPrint(gList, V, D, J, k, nav, ndv, nad5, nad3, ndd5, ndd3, naj, ndj, m, r):
         dt = datetime.now()
         r = dt.microsecond
 
-    random.seed(r)
-
     #increment variable for sucessful creations of vdj
     inc = 0
 
     #variable list pertaining to nucleotide modifications
-    #a = vd insertions
-    #b = v deletions
-    #c = d5 deletions
-    #d = d3 deletions
-    #e = vj insetions
-    #f = j deletions
+    #ndv = v deletions
+    #navd = vd insertions
+    #ndd5 = d5 deletions
+    #ndd3 = d3 deletions
+    #navj = dj insetions
+    #ndj = j deletions
     #m = mutation rate
     nucVariableList = ' '.join(['a:' + str(nav + nad5),'b:' + str(ndv),'c:' + str(ndd5),'d:' + str(ndd3),
                        'e:' + str(nad3 + naj),'f:' + str(ndj), 'm:' + str(m)])
+
+    nucVariableList = ' '.join(['ndv:' + str(ndv),'navd:' + str(nav + nad5),'ndd5:' + str(ndd5),'ndd3:' + str(ndd3),
+                       'navj:' + str(nad3 + naj),'ndj:' + str(ndj), 'm:' + str(m)])
     
     while inc < k:
 
@@ -285,11 +286,15 @@ def rPrint(gList, V, D, J, k, nav, ndv, nad5, nad3, ndd5, ndd3, naj, ndj, m, r):
 
     
 def main():
-    parser = argparse.ArgumentParser(description='Process specific alleles of V, D, and J. '
-                                     + 'With regards to nucleotide modifications the legend is as follows: '
-                                     + 'a = vd insertions, b = v deletions, c = d5 deletions, d = d3 deletions,'
-                                     + 'e = vj insertions, f = j deletions, m = mutation rate')
-    
+    parser = argparse.ArgumentParser(description="Generate VDJ recombinations with junctional diversity "
+                                     "and somatic hypermutations. Tests are in place to ensure that "
+                                     "sequences do not contain stop codons and that J gene is in the "
+                                     "correct reading frame. Default values for optional command "
+                                     "line argument are list in parentheses below. "
+                                     "Each sequence is annotated with: V allele, D allele, J allele, "
+                                     "V deletions, VD insertions, D5' deletions, D3' deletions, "
+                                     "DJ insertions, J deletions")
+
     #vdj files args
     parser.add_argument('-v', '--vgene', dest='vGene', type = validFile, required = True,
                         help = "V gene .fasta file")
@@ -303,40 +308,31 @@ def main():
 
     #nucleotide modification args
     parser.add_argument('-maxi', dest = 'maxI', type = int, default = 4,
-                        help = "max number of insertions possible for each junction, will get overrided"
-                        + "by a manual specification default = 4")
+                        help = "max # insertions possible for each junction (4)")
     parser.add_argument('-mini', dest = 'minI', type = int, default = 4,
-                        help = "min number of insertions possible for each junction, will get overrided"
-                        + "by a manual specification, default = 4")
+                        help = "min # insertions possible for each junction (4)")
     parser.add_argument('-maxd', dest = 'maxD', type = int, default = 4,
-                        help = "max number of deletions possible for each junction, will get overrided"
-                        + "by a manual specification default = 4")
+                        help = "max # deletions possible for each junction (4)")
     parser.add_argument('-mind', dest = 'minD', type = int, default = 4,
-                        help = "min number of deletions possible for each junction, will get overrided"
-                        + "by a manual specification, default = 4")
+                        help = "min # deletions possible for each junction (4)")
 
-    parser.add_argument('-navd', dest='navd', type = int, default = -1,
-                        help = "# of nucleotides to be added to the VD gap, default = -1 and will be affected"
-                        + "by maxi/mini, if a desire for the insertion to be 0 set it to 0")
+
     parser.add_argument('-ndv', dest='ndv', type = int, default = -1,
-                        help = "# of nucleotides to be deleted from the V gene, default = -1 and will be affected"
-                        + "by maxd/mind, if a desire for the deletion to be 0 set it to 0")
+                        help = "# nt to be deleted from the V gene. Set >= 0 to override maxd/mind (-1)")
+    parser.add_argument('-navd', dest='navd', type = int, default = -1,
+                        help = "# nt to be added to the VD junction. Set >= 0 to override maxi/mini (-1)")
     parser.add_argument('-ndd5', dest='ndd5', type = int, default = -1,
-                        help = "# of nucleotides to be deleted from the D gene on the 5' end, default = -1 and will be affected"
-                        + "by maxd/mind, if a desire for the deltion to be 0 set it to 0")
+                        help = "# nt to be deleted from 5' end D gene. Set >= 0 to override maxd/mind (-1)")
     parser.add_argument('-ndd3', dest='ndd3', type = int, default = -1,
-                        help = "# of nucleotides to be deleted the D gene on the 3' end, default = -1 and will be affected"
-                        + "by maxd/mind, if a desire for the deletion to be 0 set it to 0")
+                        help = "# nt to be deleted the 3' end D gene. Set >= 0 to override maxd/mind (-1)")
     parser.add_argument('-nadj', dest='nadj', type = int, default = -1,
-                        help = "# of nucleotides to be added to the DJ gap, default = -1 and will be affected"
-                        + "by by maxi/mini, if a desire for the insertion to be 0 set it to 0")
+                        help = "# nt to be added to the DJ junction. Set >= 0 to override maxi/mini (-1)")
     parser.add_argument('-ndj', dest='ndj', type = int, default = -1,
-                        help = "# of nucleotides to be deleted from the J gene, default = -1 and will be affected"
-                        + "by maxd/mind, if a desire for the deletion to be 0 set it to 0")
+                        help = "# nt to be deleted from the J gene. Set >= 0 to override maxd/mind (-1)")
     
     #misc arg variables
     parser.add_argument('-m', dest = 'mut', type = int, default = 0,
-                        help = "pct of each base mutating into another base")
+                        help = "pct of each base mutating into another base (0)")
     parser.add_argument('-r', dest='rSeed', type = int, help = "Optional random seed input"
                         " Otherwise will generate seed for the current data")
     parser.add_argument('-k', dest='kIter', type = int, required = True, help = "# of iterations")
@@ -348,14 +344,17 @@ def main():
         rNum = args.rSeed
     else:
         rNum = False
+
+    #applys random seed to every possible random var
+    random.seed(rNum)
         
     m = percentMut(args.mut)
 
     #random separator
-    vAddGap, d5AddGap = insertionDealer(args.navd, args.maxI, args.minI)
-    jAddGap, d3AddGap = insertionDealer(args.nadj, args.maxI, args.minI)
+    vAddGap, d5AddGap = insertionDealer(args.navd, args.minI, args.maxI)
+    jAddGap, d3AddGap = insertionDealer(args.nadj, args.minI, args.maxI)
 
-    delL = thresholdSet([args.ndv, args.ndd5, args.ndd3, args.ndj], args.maxD, args.minD)
+    delL = thresholdSet([args.ndv, args.ndd5, args.ndd3, args.ndj], args.minD, args.maxD)
 
     
     vFile = open(args.vGene, 'r')
